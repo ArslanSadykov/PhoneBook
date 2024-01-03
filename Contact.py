51f41e0d0978cb7a28b08f9c0e9c9c51eb8cbd6c
Sadykov Arslan <146973400+ArslanSadykov@users.noreply.github.com>
2024-01-03 19:27:07 +0300

    Delete Contact.py

--- Contact.py
+++
@@ -1,5 +0,0 @@
-class Contact:
-    def __init__(self, user, country_code, phone_number):
-        self.user = user
-        self.country_code = country_code
-        self.phone_number = phone_number
