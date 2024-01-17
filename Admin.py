091f381c139a0570515e0a58f9611ff1138d00c6
Sadykov Arslan <146973400+ArslanSadykov@users.noreply.github.com>
2024-01-17 18:14:43 +0300

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -2,6 +2,9 @@ import logging
 from User import User
 
 class Admin(User):
+    def __init__(self, firstname):
+        super().__init__(firstname)
+
     @staticmethod
     def add_contact_to_phone_book(contact, phone_book):
         phone_book.add_contact(contact)
